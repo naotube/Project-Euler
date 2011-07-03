40512b3b18732e0e98738f67c38ff84cd5473d51
Naoto Morooka <naotomorooka@gmail.com>
2011-07-03 19:23:20 +0900

    wanted to use all() function.

---
+++ euler52.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+
+    number = 1
+    while True:
+        if all(set(str(x * number)) == set(str(number)) for x in range(2,7)): 
+            break
+        number += 1
+    print(number)
