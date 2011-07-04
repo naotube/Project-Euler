3cbe71fd490749033807c85f22cb5da51ed562c0
Naoto Morooka <naotomorooka@gmail.com>
2011-07-04 11:47:03 +0900

    remove a space at the end of line

--- euler52.py
+++ euler52.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
 
     number = 1
     while True:
-        if all(set(str(x * number)) == set(str(number)) for x in range(2,7)): 
+        if all(set(str(x * number)) == set(str(number)) for x in range(2,7)):
             break
         number += 1
     print(number)
