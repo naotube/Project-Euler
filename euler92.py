9175b946a0885d3a425483f2c06d24017dc1c3ff
Naoto Morooka <naotomorooka@gmail.com>
2011-07-15 18:50:08 +0900

    wanted to keep the code simple, but slow.

--- euler92.py
+++ euler92.py
@@ -10,4 +10,19 @@ class ChainNumber:
         return self.number
 
 if __name__ == "__main__":
-    pass
+    MAXIMUM = 10000000
+    ones = 0
+    eighty_niners = 0
+
+    for i in range(1, MAXIMUM):
+        chain_number = ChainNumber(i)
+        chain_number.__iter__()
+        while True:
+            n = chain_number.__next__()
+            if n == 89:
+                eighty_niners += 1
+                break
+            if n == 1:
+                ones += 1
+                break
+    print("answer: {0}".format(eighty_niners))
