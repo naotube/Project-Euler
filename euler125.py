aeaabadbccbcca2558e13c149e0650fcba625459
Naoto Morooka <naotomorooka@gmail.com>
2011-07-29 10:08:36 +0900

    fixed range, and remove duplicate.

--- euler125.py
+++ euler125.py
@@ -1,8 +1,10 @@
 from itertools import combinations
 from math import sqrt
-LIMIT = 10 ** 3
-sums_of_consecutive_squares = (sum(i ** 2 for i in range(1,n)) for n in range(1, int(sqrt(LIMIT))))
-target_numbers = (n[1] - n[0] for n in combinations(sums_of_consecutive_squares, 2) if n[1] - n[0] < LIMIT and n[1] - n[0] == int(str(n[1] - n[0])[::-1]) and n[1] - n[0] not in (i ** 2 for i in range(1, int(sqrt(LIMIT)))))
-answer = sum([n for n in target_numbers])
+LIMIT = 10 ** 8
+sums_of_consecutive_squares = (sum(i ** 2 for i in range(n + 1)) for n in range(int(sqrt(LIMIT)) + 1))
+sums_of_consecutive_squares = (p[1] - p[0] for p in combinations(sums_of_consecutive_squares, 2) if p[1] - p[0] < LIMIT and p[1] - p[0] not in (i ** 2 for i in range(0, int(sqrt(LIMIT)))))
+
+target_numbers = (n for n in set(sums_of_consecutive_squares) if str(n) == str(n)[::-1])
+answer = sum(target_numbers)
 print(answer)
 
